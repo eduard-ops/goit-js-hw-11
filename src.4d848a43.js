37475c38078c9fd6ee0795135ea9126eac6005c9
eduard-ops <88626990+eduard-ops@users.noreply.github.com>
2022-02-12 23:07:25 +0000

    Deploying to gh-pages from @ eduard-ops/goit-js-hw-11@b775a6cbd55a5bd6ab9a88fb2926cdab39ef7b45 🚀

--- src.4d848a43.js
+++ src.4d848a43.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.cc80d4c5.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.4d848a43.js.map
