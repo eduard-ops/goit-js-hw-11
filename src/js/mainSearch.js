77404d08608b83135d5a1c50a57d1f90d9fb3a5d
eduard-ops <epustynnik@gmail.com>
2022-02-22 10:52:25 +0200

    mainSearch.js

--- src/js/mainSearch.js
+++ src/js/mainSearch.js
@@ -3,7 +3,7 @@ import GalleryApiService from './components/galleryApi';
 import Notiflix from 'notiflix';
 
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import galleryTpl from '../templates/gallery.hbs';
@@ -12,23 +12,23 @@ const refs = {
   formEl: document.querySelector('#search-form'),
   btnSubmit: document.querySelector('.btn-submit'),
   divGallery: document.querySelector('.gallery'),
-  brtMore: document.querySelector('.btn__load-more'),
+  containerEl: document.querySelector('.container'),
+  animLoad: document.querySelector('.loading'),
 };
 
-refs.brtMore.classList.add('is-hidden');
-
 refs.formEl.addEventListener('submit', onClickSubmit);
-refs.brtMore.addEventListener('click', onLoadMore);
 
 const galleryApiService = new GalleryApiService();
+
+refs.animLoad.classList.add('is-hidden');
 
 async function onClickSubmit(e) {
   e.preventDefault();
   clearHitsContainer();
-  refs.brtMore.classList.add('is-hidden');
   galleryApiService.query = e.currentTarget.elements.searchQuery.value;
   if (galleryApiService.query === '') {
     Notiflix.Notify.info('Please enter something');
+    refs.animLoad.classList.add('is-hidden');
     return;
   }
   galleryApiService.resetPage();
@@ -36,7 +36,6 @@ async function onClickSubmit(e) {
     const urlObj = await galleryApiService.fetchGallery();
     const { data } = urlObj;
     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images`);
-    refs.brtMore.classList.remove('is-hidden');
     appendHitsMarkup(data.hits);
     lightbox.refresh();
   } catch (error) {
@@ -46,30 +45,30 @@ async function onClickSubmit(e) {
   }
 }
 
-async function onLoadMore() {
-  try {
-    const urlObj = await galleryApiService.fetchGallery();
-    const {
-      data: { hits },
-    } = urlObj;
-    appendHitsMarkup(hits);
-    lightbox.refresh();
-    const { height: cardHeight } = refs.divGallery.firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
+// async function onLoadMore() {
+//   try {
+//     const urlObj = await galleryApiService.fetchGallery();
+//     const {
+//       data: { hits },
+//     } = urlObj;
+//     appendHitsMarkup(hits);
+//     lightbox.refresh();
+//     const { height: cardHeight } = refs.divGallery.firstElementChild.getBoundingClientRect();
+//     window.scrollBy({
+//       top: cardHeight * 2,
+//       behavior: 'smooth',
+//     });
 
-    if (galleryApiService.page > urlObj.data.totalHits / galleryApiService.per_page) {
-      refs.brtMore.classList.add('is-hidden');
-      return Notiflix.Notify.success('Your search has come to an end');
-    }
-  } catch (error) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.',
-    );
-  }
-}
+//     if (galleryApiService.page > urlObj.data.totalHits / galleryApiService.per_page) {
+//       refs.brtMore.classList.add('is-hidden');
+//       return Notiflix.Notify.success('Your search has come to an end');
+//     }
+//   } catch (error) {
+//     Notiflix.Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.',
+//     );
+//   }
+// }
 
 function appendHitsMarkup(hits) {
   refs.divGallery.insertAdjacentHTML('beforeend', galleryTpl(hits));
@@ -83,3 +82,29 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+const onEntry = entries => {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting && galleryApiService.query !== '') {
+      try {
+        const urlObj = await galleryApiService.fetchGallery();
+        const { data } = urlObj;
+        refs.animLoad.classList.remove('is-hidden');
+        appendHitsMarkup(data.hits);
+        lightbox.refresh();
+        if (galleryApiService.page > urlObj.data.totalHits / galleryApiService.per_page) {
+          refs.animLoad.classList.add('is-hidden');
+          return Notiflix.Notify.info('Your search has come to an end');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '50px',
+});
+
+observer.observe(refs.containerEl);
